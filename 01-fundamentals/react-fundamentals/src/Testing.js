4d1e31b4ae9fe51287b86fa3f5eafd0059071f2c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 16:06:32 -0700

    Use rfce ES7 snippet to create a regular function with export as a component

--- 01-fundamentals/react-fundamentals/src/Testing.js
+++ 01-fundamentals/react-fundamentals/src/Testing.js
@@ -1,9 +1,18 @@
 // ES7 Snippets
 // rafce (Arrow Function with Export)
+// import React from "react";
+
+// const Testing = () => {
+//   return <div>Testing</div>;
+// };
+
+// export default Testing;
+
+// rfce (Regular Function with Export)
 import React from "react";
 
-const Testing = () => {
+function Testing() {
   return <div>Testing</div>;
-};
+}
 
 export default Testing;
