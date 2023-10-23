1a8bb9c6f85f1abaa4833f486114f44c143da1cd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 16:13:00 -0700

    Use the createElement method which is being called automatically when we return JSX to better understand how React works

--- 01-fundamentals/react-fundamentals/src/index.js
+++ 01-fundamentals/react-fundamentals/src/index.js
@@ -3,8 +3,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Create a Component with a Function that Returns HTML
+// function Greeting() {
+//   return <h2>My First Component</h2>;
+// }
+
+// Use createElement method that is being called under the hood
 function Greeting() {
-  return <h2>My First Component</h2>;
+  return React.createElement("h2", {}, "Hello World!");
 }
 
 // Create a Root with ReactDOM on the Element with ID of root
