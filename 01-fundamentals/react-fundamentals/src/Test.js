fb53a266712adca4bb7031ee217d0711a0b77b0b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 16:09:32 -0700

    Delete Testing component and set ES7 snippets extension to not import React on top of every component by default, create new component Test to check it's working as desired

---
+++ 01-fundamentals/react-fundamentals/src/Test.js
@@ -0,0 +1,16 @@
+// ES7 Snippets
+// rafce (Arrow Function with Export)
+// const Test = () => {
+//   return (
+//     <div>Test</div>
+//   )
+// }
+// export default Test
+
+// rfce (Regular Function with Export)
+// function Test() {
+//   return (
+//     <div>Test</div>
+//   )
+// }
+// export default Test
