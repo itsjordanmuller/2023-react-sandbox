362d07e81b3a6949dbe9278b913680370180d24e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 16:14:19 -0700

    Wrap the h2 heading in the original Greeting component with a div to demonstrate how it would work with createElement

--- 01-fundamentals/react-fundamentals/src/index.js
+++ 01-fundamentals/react-fundamentals/src/index.js
@@ -3,14 +3,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // Create a Component with a Function that Returns HTML
-// function Greeting() {
-//   return <h2>My First Component</h2>;
-// }
+function Greeting() {
+  return (
+    // Wrap h2 heading with div for createElement demonstration
+    <div>
+      <h2>My First Component</h2>
+    </div>
+  );
+}
 
 // Use createElement method that is being called under the hood
-function Greeting() {
-  return React.createElement("h2", {}, "Hello World!");
-}
+// function Greeting() {
+//   return React.createElement("h2", {}, "Hello World!");
+// }
 
 // Create a Root with ReactDOM on the Element with ID of root
 const root = ReactDOM.createRoot(document.getElementById("root"));
