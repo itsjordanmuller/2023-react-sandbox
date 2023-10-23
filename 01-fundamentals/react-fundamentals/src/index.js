906166247d07aeb9a8d2cddad234c819a48d3ff3
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 15:48:58 -0700

    Import React & ReactDOM, create and render the root using the Greeting component to create my first React component

--- 01-fundamentals/react-fundamentals/src/index.js
+++ 01-fundamentals/react-fundamentals/src/index.js
@@ -1,4 +1,14 @@
+// Import React & ReactDOM
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 // Create a Component with a Function that Returns HTML
 function Greeting() {
   return <h2>My First Component</h2>;
 }
+
+// Create a Root with ReactDOM on the Element with ID of root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// Render the Greeting Component Using root.render(<Component-Name />)
+root.render(<Greeting />);
