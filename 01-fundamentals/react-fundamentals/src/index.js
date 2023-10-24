fafa636e5d5dc9dcffcb007a18046443c9a94313
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 21:32:59 -0700

    Show some other options for styling components using a JavaScript object

--- 01-fundamentals/react-fundamentals/src/index.js
+++ 01-fundamentals/react-fundamentals/src/index.js
@@ -36,11 +36,17 @@ const Title = () => (
   </h2>
 );
 const Author = () => {
+  const inlineHeadingStyles = {
+    color: "#617d98",
+    fontSize: "0.75rem",
+    marginTop: "0.5rem",
+  };
   return (
-    <h4
+    <h4 className="book-author" style={inlineHeadingStyles}>
+      {/* <h4
       className="book-author"
-      style={{ color: "#617d98", fontSize: "0.75", marginTop: "0.5rem" }}
-    >
+      style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
+    > */}
       David Brooks
     </h4>
   );
