17a17e20f79e7b13ae4a716da71be629fe0830f9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-23 21:37:42 -0700

    Refactor Book component in index.js from using multiple nested components to a single component

--- 01-fundamentals/react-fundamentals/src/index.js
+++ 01-fundamentals/react-fundamentals/src/index.js
@@ -16,41 +16,51 @@ const BookList = () => {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        src="./images/how-to-know-a-person-cover.jpg"
+        alt="How to Know a Person Book Cover Art"
+        className="book-img"
+      />
+      <h2 className="book-title">
+        How to Know a Person: The Art of Seeing Others Deeply and Being Deeply
+        Seen
+      </h2>
+      <h4 className="book-author">David Brooks</h4>
     </article>
   );
 };
 
-const Image = () => (
-  <img
-    src="./images/how-to-know-a-person-cover.jpg"
-    alt="How to Know a Person Book Cover Art"
-    className="book-img"
-  />
-);
-const Title = () => (
-  <h2 className="book-title">
-    How to Know a Person: The Art of Seeing Others Deeply and Being Deeply Seen
-  </h2>
-);
-const Author = () => {
-  const inlineHeadingStyles = {
-    color: "#617d98",
-    fontSize: "0.75rem",
-    marginTop: "0.5rem",
-  };
-  return (
-    <h4 className="book-author" style={inlineHeadingStyles}>
-      {/* <h4
-      className="book-author"
-      style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
-    > */}
-      David Brooks
-    </h4>
-  );
-};
+// const Image = () => (
+//   <img
+//     src="./images/how-to-know-a-person-cover.jpg"
+//     alt="How to Know a Person Book Cover Art"
+//     className="book-img"
+//   />
+// );
+
+// const Title = () => (
+//   <h2 className="book-title">
+//     How to Know a Person: The Art of Seeing Others Deeply and Being Deeply Seen
+//   </h2>
+// );
+
+// const Author = () => {
+//   const inlineHeadingStyles = {
+//     color: "#617d98",
+//     fontSize: "0.75rem",
+//     marginTop: "0.5rem",
+//   };
+//   return (
+//     <h4 className="book-author" style={inlineHeadingStyles}>
+//       {/* <h4
+//       className="book-author"
+//       style={{ color: "#617d98", fontSize: "0.75rem", marginTop: "0.5rem" }}
+//     > */}
+//       David Brooks
+//     </h4>
+//   );
+// };
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(<BookList />);
