ffb3b1600f819671a76ca5f0d54c4f5879ff72aa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-24 14:50:59 -0700

    Import images from src folder instead of public to optimize them

--- 01-fundamentals/react-fundamentals/src/books.js
+++ 01-fundamentals/react-fundamentals/src/books.js
@@ -1,18 +1,29 @@
+import img1 from "./images/how-to-know-a-person-cover.jpg";
+import img2 from "./images/atomic-habits-cover.jpg";
+import img3 from "./images/hidden-potential-cover.jpg";
 export const books = [
   {
     id: 1,
     title:
       "How to Know a Person: The Art of Seeing Others Deeply and Being Deeply Seen",
     author: "David Brooks",
-    img: "/images/how-to-know-a-person-cover.jpg",
+    img: img1,
     alt: "How to Know a Person Book Cover Art",
   },
   {
     id: 2,
-    title: "Atomic Habits",
+    title:
+      "Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones",
     author: "James Clear",
-    img: "/images/atomic-habits-cover.jpg",
+    img: img2,
     alt: "Atomic Habits Book Cover Art",
+  },
+  {
+    id: 3,
+    title: "Hidden Potential: The Science of Achieving Greater Things",
+    author: "Adam Grant",
+    img: img3,
+    alt: "Hidden Potential Book Cover Art",
   },
 ];
 
