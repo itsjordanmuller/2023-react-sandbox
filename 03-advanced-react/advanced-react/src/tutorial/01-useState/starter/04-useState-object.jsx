cf6f8275517d9ad888af346d0e50cd7126979f97
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-25 16:45:37 -0700

    Create a button that uses the State Hook to set Name, Age, and Hobby values and re-render

--- 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,7 +1,26 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [name, setName] = useState("peter");
+  const [age, setAge] = useState(24);
+  const [hobby, setHobby] = useState("programming");
+
+  const displayPerson = () => {
+    setName("john");
+    setAge(28);
+    setHobby("reading");
+  };
+
+  return (
+    <>
+      <h3>{name}</h3>
+      <h3>{age}</h3>
+      <h4>Enjoys: {hobby}</h4>
+      <button className="btn" type="button" onClick={displayPerson}>
+        Cycle
+      </button>
+    </>
+  );
 };
 
 export default UseStateObject;
