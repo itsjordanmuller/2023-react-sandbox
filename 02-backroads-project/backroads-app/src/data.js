18f0fccba11868289bd487fc145f90f699b4762e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-24 22:12:21 -0700

    Modify original tours section HTML component to get dynamic values from data file

--- 02-backroads-project/backroads-app/src/data.js
+++ 02-backroads-project/backroads-app/src/data.js
@@ -1,3 +1,8 @@
+import tour1 from "./images/tour-1.jpeg";
+import tour2 from "./images/tour-2.jpeg";
+import tour3 from "./images/tour-3.jpeg";
+import tour4 from "./images/tour-4.jpeg";
+
 export const pageLinks = [
   {
     id: 1,
@@ -57,5 +62,48 @@ export const services = [
     icon: "fa-socks",
     title: "amazing comfort",
     text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
+
+export const tours = [
+  {
+    id: 1,
+    img: tour1,
+    date: "august 26th, 2020",
+    title: "tibet adventure",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "china",
+    duration: 6,
+    cost: 2100,
+  },
+  {
+    id: 2,
+    img: tour2,
+    date: "october 1th, 2020",
+    title: "best of java",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "indonesia",
+    duration: 11,
+    cost: 1400,
+  },
+  {
+    id: 3,
+    img: tour3,
+    date: "september 15th, 2020",
+    title: "explore hong kong",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "hong kong",
+    duration: 8,
+    cost: 5000,
+  },
+  {
+    id: 4,
+    img: tour4,
+    date: "december 5th, 2019",
+    title: "kenya highlights",
+    info: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque vitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.",
+    location: "kenya",
+    duration: 20,
+    cost: 3300,
   },
 ];
