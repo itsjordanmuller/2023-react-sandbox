310c533cc29ade4617efcd243b374d02a9bf04e2
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-25 16:25:27 -0700

    Map an array of objects in data file in State Hook array example, begin to implement full functionality of challenge

--- 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/03-useState-array.jsx
@@ -1,5 +1,22 @@
+import { data } from "../../../data";
+import React from "react";
+
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data);
+
+  return (
+    <div>
+      {people.map((person) => {
+        const { id, name } = person;
+        // console.log(person);
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default UseStateArray;
