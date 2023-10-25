b752e5bcae0f67499ba899ea3580f325b265bc0c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-25 13:49:27 -0700

    Attempt to display an incrementing count without re-rendering the pagevia a button click
    This won't work since the page never re-renders, the value increments in the console but not on the page

--- 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,20 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let count = 0;
+
+  const increaseCount = () => {
+    count = count + 1;
+    console.log(count);
+  };
+
+  return (
+    <div>
+      <h2>useState error example</h2>
+      <h3>Count: {count}</h3>
+      <button type="button" onClick={increaseCount}>
+        Increase
+      </button>
+    </div>
+  );
 };
 
 export default ErrorExample;
