d781fa3fd8cad33e7279ebd28e1b45dbbccc8d8c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-25 13:58:37 -0700

    Import and use the State Hook, use it to fix the error in our initial attempt to create an incrementing value that re-renders on click

--- 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+  // console.log(useState(1));
+  // const value = useState("hello")[0];
+  // const func = useState("hello")[1];
+  // console.log(value);
+  // console.log(func);
+
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <>
+      <h2>useState basics</h2>
+      <h3>Count: {count}</h3>
+      <button type="button" onClick={increaseCount}>
+        Increase
+      </button>
+    </>
+  );
 };
 
 export default UseStateBasics;
