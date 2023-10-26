5e9719f79a028e4b138a367071be60316e905909
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 15:20:12 -0700

    Complete a practical example of multiple returns with a loading return, error return, and user profile return on a GitHub API fetch

--- 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,10 @@
 import { useEffect, useState } from "react";
-const url = "https://api.github.com/users/QuincyLarson";
+const url = "https://api.github.com/users/itsjordanmuller";
 
 const MultipleReturnsFetchData = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+
   const [user, setUser] = useState(null);
 
   useEffect(() => {
@@ -10,13 +13,38 @@ const MultipleReturnsFetchData = () => {
         const response = await fetch(url);
         const user = await response.json();
         console.log(user);
+        setUser(user);
       } catch (error) {
+        setIsError(true);
         console.log(error);
       }
+      setIsLoading(false);
     };
     getUser();
   }, []);
 
-  return <h2>Fetch Example</h2>;
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (isError) {
+    return <h2>There was an error...</h2>;
+  }
+
+  return (
+    <div className="container">
+      <h2>Fetch Example</h2>
+      <img
+        style={{ width: "150px", borderRadius: "25px" }}
+        src={user.avatar_url}
+        alt={user.name}
+      />
+      <h2>{user.name}</h2>
+      <p>{user.bio}</p>
+      <a href={user.html_url} className="btn">
+        View GitHub
+      </a>
+    </div>
+  );
 };
 export default MultipleReturnsFetchData;
