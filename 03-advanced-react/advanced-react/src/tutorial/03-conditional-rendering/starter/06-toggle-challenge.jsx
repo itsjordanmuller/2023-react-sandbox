1b6f3ea552c632fa97399f77cc0463a65fd9566c
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 16:04:52 -0700

    Toggle state value when user clicks button to conditionally render component/elements

--- 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/06-toggle-challenge.jsx
@@ -1,5 +1,31 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
 const ToggleChallenge = () => {
-  return <h2>toggle challenge</h2>;
+  const [showAlert, setShowAlert] = useState(false);
+
+  const toggleAlert = () => {
+    if (showAlert) {
+      setShowAlert(false);
+      return;
+    }
+    setShowAlert(true);
+  };
+
+  return (
+    <>
+      <h2>toggle challenge</h2>
+      <div>
+        <button className="btn" onClick={toggleAlert}>
+          toggle alert
+        </button>
+        {showAlert && <Alert />}
+      </div>
+    </>
+  );
+};
+const Alert = () => {
+  return <div className="alert alert-danger">Alert!</div>;
 };
 
 export default ToggleChallenge;
