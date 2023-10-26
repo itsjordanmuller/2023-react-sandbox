56ed826f5f7d2515fbdebe56b4bb4a93a295c008
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-25 17:06:56 -0700

    Show how multiple clicks can be logged within a setTimeout instead of only incrementing after timeout

--- 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -3,13 +3,26 @@ import { useState } from "react";
 const UseStateGotcha = () => {
   const [value, setValue] = useState(0);
 
+  // Will Not Allow User to Click 5x in 3s to Get +5
+  // setTimeout(() => {
+  //   setValue(value + 1);
+  // }, 3000);
+
+  // A Way Around setTimeout Edge Case
   const handleClick = () => {
+    setTimeout(() => {
+      console.log("clicked the button!");
+      setValue((currentState) => {
+        return currentState + 1;
+      });
+    }, 3000);
+
     // setValue(value + 1);
-    setValue((currentState) => {
-      const newState = currentState + 1;
-      console.log(newState);
-      return newState;
-    });
+    // setValue((currentState) => {
+    //   const newState = currentState + 1;
+    //   console.log(newState);
+    //   return newState;
+    // });
     // console.log(value);
   };
   return (
