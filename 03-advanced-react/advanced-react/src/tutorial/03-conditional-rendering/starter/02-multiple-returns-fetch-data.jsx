20735cda65bef1cdc3a266e6be02f60574da79dd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 15:08:02 -0700

    Setup the State and Effect Hooks to use async await to fetch a user from GitHub API

--- 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/02-multiple-returns-fetch-data.jsx
@@ -1,7 +1,22 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import { useEffect, useState } from "react";
+const url = "https://api.github.com/users/QuincyLarson";
 
 const MultipleReturnsFetchData = () => {
-  return <h2>Fetch Data </h2>;
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await fetch(url);
+        const user = await response.json();
+        console.log(user);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getUser();
+  }, []);
+
+  return <h2>Fetch Example</h2>;
 };
 export default MultipleReturnsFetchData;
