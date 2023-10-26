a514618f5f78a18ae1cb935c37c31815165bef52
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 15:42:54 -0700

    Use short-circuit evaluation in JavaScript to observe how truthy and falsy values behave in terms of State Hook

--- 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/03-conditional-rendering/starter/04-short-circuit-overview.jsx
@@ -1,6 +1,27 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 const ShortCircuitOverview = () => {
-  return <h2>short circuit overview</h2>;
+  // Truthy
+  const [name, setName] = useState("jordan");
+
+  // Falsy
+  const [text, setText] = useState("");
+
+  const codeExample = text || "hello world";
+
+  return (
+    <>
+      <h2>short circuit overview</h2>
+      <div>
+        {/* { if(someCondition){"wont work"}} */}
+        <h4>Falsy OR: {text || "hello world"}</h4>
+        <h4>Falsy AND: {text && "hello world"}</h4>
+        <h4>Truthy OR: {name || "hello world"}</h4>
+        <h4>Truthy AND: {name && "hello world"}</h4>
+        <br />
+        <h4>{codeExample}</h4>
+      </div>
+    </>
+  );
 };
 export default ShortCircuitOverview;
