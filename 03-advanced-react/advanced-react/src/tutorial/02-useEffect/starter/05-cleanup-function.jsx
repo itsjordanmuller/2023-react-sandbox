b84869796ac860e915f052a5a2485f10f020ef35
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 16:17:24 -0700

    Show how an Effect Hook that should only trigger on render, is still shown each time the component is mounted

--- 03-advanced-react/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/02-useEffect/starter/05-cleanup-function.jsx
@@ -1,5 +1,23 @@
+import { useState, useEffect } from "react";
+
 const CleanupFunction = () => {
-  return <h2>cleanup function</h2>;
+  const [toggle, setToggle] = useState(false);
+  return (
+    <>
+      <h2>cleanup function</h2>
+      <button onClick={() => setToggle(!toggle)} className="btn">
+        Toggle
+      </button>
+      {toggle && <RandomComponent />}
+    </>
+  );
+};
+
+const RandomComponent = () => {
+  useEffect(() => {
+    console.log("What's going on here?");
+  }, []);
+  return <h1>hello there</h1>;
 };
 
 export default CleanupFunction;
