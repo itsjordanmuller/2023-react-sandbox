876b5604858129af7f33a67b46b77a9db2b0a502
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 16:30:00 -0700

    Showcase another way to export multiple components using index.jsx exporting to a named component

--- 03-advanced-react/advanced-react/src/App.jsx
+++ 03-advanced-react/advanced-react/src/App.jsx
@@ -1,12 +1,11 @@
 // import Starter from "./tutorial/04-project-structure/Pages";
-import { Home, About } from "./tutorial/04-project-structure/Pages";
+import Example from "./tutorial/04-project-structure/Example";
 
 function App() {
   return (
     <div className="container">
       {/* <Starter /> */}
-      <Home />
-      <About />
+      <Example />
     </div>
   );
 }
