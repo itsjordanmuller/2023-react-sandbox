fde108384d56061c7b88f1eec968ac2597e311b5
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 22:25:23 -0700

    Add a button to display a random review, add 1 to the index if the random value is the same as the current index

--- 04-fundamental-projects/03-reviews/reviews/src/App.jsx
+++ 04-fundamental-projects/03-reviews/reviews/src/App.jsx
@@ -25,9 +25,15 @@ const App = () => {
   const prevPerson = () => {
     setIndex((currentIndex) => {
       const newIndex = currentIndex - 1;
-      checkNumber(currentIndex);
       return checkNumber(newIndex);
     });
+  };
+  const randomPerson = () => {
+    let randomIndex = Math.floor(Math.random() * people.length);
+    if (randomIndex === index) {
+      randomIndex = index + 1;
+    }
+    setIndex(checkNumber(randomIndex));
   };
 
   // console.log(name);
@@ -51,6 +57,9 @@ const App = () => {
             <FaChevronRight />
           </button>
         </div>
+        <button className="btn btn-hipster" onClick={randomPerson}>
+          Random Review
+        </button>
       </article>
     </main>
   );
