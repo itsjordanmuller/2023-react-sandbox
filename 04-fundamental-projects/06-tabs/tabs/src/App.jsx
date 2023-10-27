d8ff455b49c50380ae788369e6c24d1e30f17821
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 14:53:41 -0700

    Fetch the jobs data from the course-api using Effect Hook and asynchronous fetch to API url, log jobs data to console

--- 04-fundamental-projects/06-tabs/tabs/src/App.jsx
+++ 04-fundamental-projects/06-tabs/tabs/src/App.jsx
@@ -1,6 +1,24 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [jobs, setJobs] = useState([]);
+
+  const fetchJobs = async () => {
+    const response = await fetch(url);
+    const newJobs = await response.json();
+    setJobs(newJobs);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  console.log(jobs);
+
   return <h2>Tabs Starter</h2>;
 };
 export default App;
