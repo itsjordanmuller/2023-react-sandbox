8d2200deab2cf685b57b535ac899315e115506fa
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 16:52:24 -0700

    Show how we can leverage JavaScript and use chaining to return a default or found value

--- 03-advanced-react/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/05-leverage-javascript/starter/Person.jsx
@@ -1,9 +1,14 @@
-const Person = ({ name, nickName, images }) => {
-  const img = images[0].small.url;
+import avatar from "../../../assets/default-avatar.svg";
+
+const Person = ({ name, nickName = "None Given", images }) => {
+  // Long Way
+  // const img = images && images[0] && images[0].small && images[0].small.url;
+  // Short Way Using Optional Chaining
+  const img = images?.[0]?.small?.url ?? avatar;
 
   return (
     <div>
-      <img src={img} alt={name} style={{ width: "50px" }} />
+      <img src={img} alt={name} style={{ width: "50px", height: "50px" }} />
       <h4>{name}</h4>
       <p>Nickname: {nickName}</p>
     </div>
