83e3c92de0e826d9620059b654f50484c4e2d699
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 15:09:30 -0700

    Assign a UUIDV4 (Universally Unique Identifier) to each duty instead of relying on index

--- 04-fundamental-projects/06-tabs/tabs/src/Duties.jsx
+++ 04-fundamental-projects/06-tabs/tabs/src/Duties.jsx
@@ -1,11 +1,14 @@
 import { FaAngleDoubleRight } from "react-icons/fa";
+import { v4 as uuidv4 } from "uuid";
 
 const Duties = ({ duties }) => {
   return (
     <div>
       {duties.map((duty, index) => {
+        const id = uuidv4();
+        // console.log(id);
         return (
-          <div key={index} className="job-desc">
+          <div key={id} className="job-desc">
             <FaAngleDoubleRight className="job-icon" />
             <p>{duty}</p>
           </div>
