27ad1e88d4c9fbc3f3227db1862981d5f00ee058
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 14:28:25 -0700

    Get unique categories including an 'all' category for state initialization for filter categories

--- 04-fundamental-projects/05-menu/menu/src/App.jsx
+++ 04-fundamental-projects/05-menu/menu/src/App.jsx
@@ -3,8 +3,18 @@ import menu from "./data";
 import Title from "./Title";
 import Menu from "./Menu";
 
+// Long Way of Getting Unique Categories Using Set
+// const tempCategories = menu.map((item) => item.category);
+// const tempSet = new Set(tempCategories);
+// const tempItems = ["all", ...tempSet];
+// console.log(tempItems);
+
+// Short/One Line Way of Getting Unique Categories Using Set
+const allCategories = ["all", ...new Set(menu.map((item) => item.category))];
+
 const App = () => {
   const [menuItems, setMenuItems] = useState(menu);
+  const [categories, setCategories] = useState(allCategories);
   return (
     <main>
       <section className="menu">
