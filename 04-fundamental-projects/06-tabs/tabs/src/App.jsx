e7312b9ff6c7fedb58e6e942c957c3055fb2db45
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 15:00:24 -0700

    Display the details of the first job in the array in the JobInfo component

--- 04-fundamental-projects/06-tabs/tabs/src/App.jsx
+++ 04-fundamental-projects/06-tabs/tabs/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import JobInfo from "./JobInfo";
 
 const url = "https://course-api.com/react-tabs-project";
 
@@ -25,6 +26,10 @@ const App = () => {
     );
   }
 
-  return <h2>Tabs Starter</h2>;
+  return (
+    <section className="jobs-center">
+      <JobInfo jobs={jobs} />
+    </section>
+  );
 };
 export default App;
