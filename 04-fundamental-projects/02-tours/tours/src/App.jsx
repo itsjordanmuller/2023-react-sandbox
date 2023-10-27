603cb47166794f351ed3ce4dd4e20fc1f5f4ca11
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-26 21:03:17 -0700

    Fetch tours from the course-api, set loading state to true

--- 04-fundamental-projects/02-tours/tours/src/App.jsx
+++ 04-fundamental-projects/02-tours/tours/src/App.jsx
@@ -1,6 +1,26 @@
-const url = 'https://course-api.com/react-tours-project';
+import { useEffect, useState } from "react";
+
+const url = "https://course-api.com/react-tours-project";
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [tours, setTours] = useState(true);
+
+  const fetchTours = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      console.log(tours);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
   return <h2>Tours Starter</h2>;
 };
 export default App;
