e771f3ca9fb6bbec6a01c1f522ac711a9117d1f9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-27 15:33:43 -0700

    Begin to layout the layout for a single slide with an image, name, title, text, and quote icon

---
+++ 04-fundamental-projects/07-slider/slider/src/Carousel.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { shortList, list, longList } from "./data";
+import { FaQuoteRight } from "react-icons/fa";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+
+const Carousel = () => {
+  const [people, setPeople] = useState(shortList);
+  return (
+    <section className="slider-container">
+      {people.map((person) => {
+        const { id, image, name, title, quote } = person;
+        return (
+          <article className="slide" key={id}>
+            <img src={image} alt={name} className="person-img" />
+            <h5 className="name">{name}</h5>
+            <p className="title">{title}</p>
+            <p className="text">{quote}</p>
+            <FaQuoteRight className="icon" />
+          </article>
+        );
+      })}
+    </section>
+  );
+};
+export default Carousel;
