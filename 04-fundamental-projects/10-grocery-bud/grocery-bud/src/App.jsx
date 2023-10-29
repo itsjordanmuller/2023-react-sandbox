60ff56165ba040bd1dd4c3ca98b43f4d7cd01a6e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-28 23:07:27 -0700

    Implement the ability to track the checked status of the items using local storage instead of State Hook

--- 04-fundamental-projects/10-grocery-bud/grocery-bud/src/App.jsx
+++ 04-fundamental-projects/10-grocery-bud/grocery-bud/src/App.jsx
@@ -20,8 +20,7 @@ const setLocalStorage = (items) => {
 const defaultList = JSON.parse(localStorage.getItem("list") || "[]");
 
 const App = () => {
-  getLocalStorage();
-  const [items, setItems] = useState(getLocalStorage());
+  const [items, setItems] = useState(defaultList);
 
   const addItem = (itemName) => {
     const newItem = {
@@ -40,10 +39,22 @@ const App = () => {
     setLocalStorage(newItems);
   };
 
+  const editItem = (itemId) => {
+    const newItems = items.map((item) => {
+      if (item.id === itemId) {
+        const newItem = { ...item, completed: !item.completed };
+        return newItem;
+      }
+      return item;
+    });
+    setItems(newItems);
+    setLocalStorage(newItems);
+  };
+
   return (
     <section className="section-center">
       <Form addItem={addItem} />
-      <Items items={items} removeItem={removeItem} />
+      <Items items={items} removeItem={removeItem} editItem={editItem} />
     </section>
   );
 };
