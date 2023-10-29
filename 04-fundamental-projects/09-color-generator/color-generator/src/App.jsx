fec1e83fde81626abfa847ac5c04671ececb9e25
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-28 17:48:57 -0700

    Import and setup react-toastify ToastContainer component

--- 04-fundamental-projects/09-color-generator/color-generator/src/App.jsx
+++ 04-fundamental-projects/09-color-generator/color-generator/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import ColorList from "./ColorList";
 import Form from "./Form";
 import Values from "values.js";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
   const [colors, setColors] = useState(new Values("#8e44ad").all(10));
@@ -10,6 +11,7 @@ const App = () => {
     <main>
       <Form />
       <ColorList colors={colors} />
+      <ToastContainer position="top-center" />
     </main>
   );
 };
