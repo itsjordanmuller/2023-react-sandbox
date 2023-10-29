5bd4492620d3ed745b418cf6d82e1f6e656acb6e
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-28 17:13:50 -0700

    Implement the ability for the lorem ipsum generator to show between 1 and 8 paragraphs of lorem text

--- 04-fundamental-projects/08-lorem-ipsum/lorem-ipsum/src/App.jsx
+++ 04-fundamental-projects/08-lorem-ipsum/lorem-ipsum/src/App.jsx
@@ -4,10 +4,17 @@ import data from "./data";
 const App = () => {
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setText(data.slice(0, count));
+  };
+
   return (
     <section className="section-center">
       <h4>Tired of Boring Lorem Ipsum Text?</h4>
-      <form className="lorem-form">
+      <form className="lorem-form" onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs: </label>
         <input
           type="number"
@@ -17,12 +24,17 @@ const App = () => {
           max="8"
           step="1"
           value={count}
-          onChange={(e) => setCount(e.target.value)}
+          onChange={(e) => setCount(parseInt(e.target.value))}
         />
         <button className="btn" type="submit">
           Generate
         </button>
       </form>
+      <article className="lorem-text">
+        {text.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
     </section>
   );
 };
