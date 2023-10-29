f093e2f7c276f46ed014ea6573d34e6b8f06d4ce
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-28 17:58:41 -0700

    Use the clipboard API to save the colors to the user clipboard when the click any of the SingleColor components

--- 04-fundamental-projects/09-color-generator/color-generator/src/SingleColor.jsx
+++ 04-fundamental-projects/09-color-generator/color-generator/src/SingleColor.jsx
@@ -1,9 +1,24 @@
+import { toast } from "react-toastify";
+
 const SingleColor = ({ index, color }) => {
   const { hex, weight } = color;
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success("Color Copied to Clipboard");
+      } catch (error) {
+        toast.error("Failed to Copy Color to Clipboard");
+      }
+    } else {
+      toast.error("Clipboard Access Unavailable");
+    }
+  };
   return (
     <article
       className={index > 10 ? "color color-light" : "color"}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className="percent-value">{weight}%</p>
       <p className="color-value">#{hex}</p>
