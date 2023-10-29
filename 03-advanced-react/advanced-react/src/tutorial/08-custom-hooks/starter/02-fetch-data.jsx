e48d4bbe5194c32ba715aecb1fa45e035fbccf5d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-29 15:56:46 -0700

    Create a useFetchPerson Hook to fetch a person at a given URL from the GitHub API, uses State and Effect Hooks inside

--- 03-advanced-react/advanced-react/src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/08-custom-hooks/starter/02-fetch-data.jsx
@@ -1,33 +1,9 @@
-import { useEffect, useState } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import useFetchUser from "./useFetchPerson";
+
+const url = "https://api.github.com/users/itsjordanmuller";
 
 const FetchData = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const [isError, setIsError] = useState(false);
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const resp = await fetch(url);
-        // console.log(resp);
-        if (!resp.ok) {
-          setIsError(true);
-          setIsLoading(false);
-          return;
-        }
-
-        const user = await resp.json();
-        setUser(user);
-      } catch (error) {
-        setIsError(true);
-        // console.log(error);
-      }
-      // hide loading
-      setIsLoading(false);
-    };
-    fetchUser();
-  }, []);
+  const { isLoading, isError, user } = useFetchUser(url);
   // order matters
   // don't place user JSX before loading or error
 
@@ -37,16 +13,20 @@ const FetchData = () => {
   if (isError) {
     return <h2>There was an error...</h2>;
   }
-  const { avatar_url, name, company, bio } = user;
+  const { avatar_url, name, html_url, bio } = user;
   return (
     <div>
       <img
-        style={{ width: '100px', borderRadius: '25px' }}
+        style={{ width: "100px", borderRadius: "25px" }}
         src={avatar_url}
         alt={name}
       />
       <h2>{name}</h2>
-      <h4>works at {company}</h4>
+      <h4>
+        <a href={html_url}>
+          <button className="btn">Link to Profile</button>
+        </a>
+      </h4>
       <p>{bio}</p>
     </div>
   );
