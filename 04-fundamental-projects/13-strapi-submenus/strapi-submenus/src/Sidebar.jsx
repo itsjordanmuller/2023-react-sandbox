b48109d4f6ff461cf933376566d4cc80f6e027c8
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-29 19:57:19 -0700

    Map the sublinks to get all of the pages, take the links array in the sublinks objects and map each link as a link icon with a label

--- 04-fundamental-projects/13-strapi-submenus/strapi-submenus/src/Sidebar.jsx
+++ 04-fundamental-projects/13-strapi-submenus/strapi-submenus/src/Sidebar.jsx
@@ -1,4 +1,39 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./Context";
+import sublinks from "./data";
+
 const Sidebar = () => {
-  return <h2>Sidebar</h2>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-container">
+        <button className="close-btn" onClick={closeSidebar}>
+          <FaTimes />
+        </button>
+        <div className="sidebar-links">
+          {sublinks.map((item) => {
+            const { links, page, pageId } = item;
+            console.log(item);
+            return (
+              <article key={pageId}>
+                <h4>{page}</h4>
+                <div className="sidebar-sublinks">
+                  {links.map((link) => {
+                    const { url, icon, label, id } = link;
+                    return (
+                      <a key={id} href={url}>
+                        {icon}
+                        {label}
+                      </a>
+                    );
+                  })}
+                </div>
+              </article>
+            );
+          })}
+        </div>
+      </div>
+    </aside>
+  );
 };
 export default Sidebar;
