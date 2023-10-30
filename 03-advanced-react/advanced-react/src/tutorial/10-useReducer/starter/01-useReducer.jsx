6968154bb498a91e76ce29df25d5faa80c0e0549
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-29 22:21:23 -0700

    Replace action type strings with constant variables to avoid errors, throw new Error when no matching action type is found

--- 03-advanced-react/advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
+++ 03-advanced-react/advanced-react/src/tutorial/10-useReducer/starter/01-useReducer.jsx
@@ -6,26 +6,34 @@ const defaultState = {
   isLoading: false,
 };
 
+const CLEAR_LIST = "CLEAR_LIST";
+const RESET_LIST = "RESET_LIST";
+const REMOVE_ITEM = "REMOVE_ITEM";
+
 const reducer = (state, action) => {
-  if (action.type === "CLEAR_LIST") {
+  if (action.type === CLEAR_LIST) {
     return { ...state, people: [] };
   }
+  // return state;
+  throw new Error(`No matching "${action.type}" - action type`);
 };
 
 const ReducerBasics = () => {
   const [state, dispatch] = useReducer(reducer, defaultState);
 
   const removeItem = (id) => {
+    dispatch({ type: REMOVE_ITEM });
     // let newPeople = people.filter((person) => person.id !== id);
     // setPeople(newPeople);
   };
 
   const clearList = () => {
-    dispatch({ type: "CLEAR_LIST" });
+    dispatch({ type: CLEAR_LIST });
     // setPeople([]);
   };
 
   const resetList = () => {
+    dispatch({ type: RESET_LIST });
     // setPeople(data);
   };
 
