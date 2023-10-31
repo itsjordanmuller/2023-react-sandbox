a8784e7e46e0484dc0a01d7aa87a7a8a8ae27b9b
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-30 19:51:48 -0700

    Send a get request to the baseURL using the customFetch Axios Custom Instance

--- 06-react-query/starter/src/Items.jsx
+++ 06-react-query/starter/src/Items.jsx
@@ -1,7 +1,15 @@
-import SingleItem from './SingleItem';
+import SingleItem from "./SingleItem";
+import { useQuery } from "@tanstack/react-query";
+import customFetch from "./utils";
+
 const Items = ({ items }) => {
+  const result = useQuery({
+    queryKey: ["tasks"],
+    queryFn: () => customFetch.get("/"),
+  });
+  console.log(result);
   return (
-    <div className='items'>
+    <div className="items">
       {items.map((item) => {
         return <SingleItem key={item.id} item={item} />;
       })}
