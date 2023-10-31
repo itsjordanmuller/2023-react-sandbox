095460a29fa73dbd61bbc244788a21eb7ba98ccd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 15:39:10 -0700

    Work on the Hero component, add a heading, paragraph text, and an SVG image

--- 08-contentful-cms/contentful/src/Hero.jsx
+++ 08-contentful-cms/contentful/src/Hero.jsx
@@ -1,4 +1,27 @@
+import heroImg from "./assets/hero.svg";
+
 const Hero = () => {
-  return <h2>Hero</h2>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+            eveniet quaerat delectus, ea illum, iusto hic deserunt nihil eos
+            quis laudantium assumenda quibusdam quam, cupiditate a maxime alias
+            pariatur.
+          </p>
+        </div>
+        <div className="img-container">
+          <img
+            src={heroImg}
+            alt="person interacting with abstract web browser"
+            className="img"
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 export default Hero;
