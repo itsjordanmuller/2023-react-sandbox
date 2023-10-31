6d16e072084099515148dfdbe3cafe8a5fe42907
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 13:55:44 -0700

    Remove greeting test function from context and AppContext Provider value

--- 07-unsplash-images/stock-images/src/context.jsx
+++ 07-unsplash-images/stock-images/src/context.jsx
@@ -3,11 +3,7 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
-
-  return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
-  );
+  return <AppContext.Provider value={{}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(AppContext);
