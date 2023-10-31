186012c669ab3c8d62f4c89386ccc0100e88bf84
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 16:14:01 -0700

    Create a custom FetchProjects Hook to fetch and properly structure our data from Contentful to prepare it for rendering

--- 08-contentful-cms/contentful/src/Projects.jsx
+++ 08-contentful-cms/contentful/src/Projects.jsx
@@ -1,6 +1,9 @@
-import "./fetchProjects.jsx";
+import { useFetchProjects } from "./fetchProjects";
 
 const Projects = () => {
+  const { loading, projects } = useFetchProjects();
+  console.log(loading, projects);
   return <h2>Projects</h2>;
 };
+
 export default Projects;
--- 08-contentful-cms/contentful/src/fetchProjects.jsx
+++ 08-contentful-cms/contentful/src/fetchProjects.jsx
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const SPACE_ID = import.meta.env.VITE_CONTENTFUL_SPACE_ID;
 const ACCESS_TOKEN = import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN;
@@ -9,6 +11,32 @@ const client = createClient({
   accessToken: ACCESS_TOKEN,
 });
 
-client
-  .getEntries({ content_type: "exampleProjects" })
-  .then((response) => console.log(response));
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({
+        content_type: "exampleProjects",
+      });
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const id = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return { title, url, id, img };
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return { loading, projects };
+};
