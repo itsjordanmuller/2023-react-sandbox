6fd44345edcfca441b1487bf2dff278ddbaa6d98
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 22:36:33 -0700

    Handle submitting state with Navigation React Query Hook

--- 09-mixmaster/mixmaster/src/pages/Newsletter.jsx
+++ 09-mixmaster/mixmaster/src/pages/Newsletter.jsx
@@ -1,4 +1,4 @@
-import { Form, redirect } from "react-router-dom";
+import { Form, redirect, useNavigation } from "react-router-dom";
 import axios from "axios";
 import { toast } from "react-toastify";
 
@@ -19,6 +19,8 @@ export const action = async ({ request }) => {
 };
 
 const Newsletter = () => {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
   return (
     <Form className="form" method="POST">
       <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>Newsletter</h4>
@@ -66,8 +68,9 @@ const Newsletter = () => {
         type="submit"
         className="btn btn-block"
         style={{ marginTop: "0.5rem" }}
+        disabled={isSubmitting}
       >
-        Submit
+        {isSubmitting ? "Submitting" : "Submit"}
       </button>
     </Form>
   );
