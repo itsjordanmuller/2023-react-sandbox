aa7008fc511c6d1b32f36d0276144e9b328b83ef
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 17:41:58 -0700

    Setup all of the pages as seperate components in src/pages folder, export all pages using index file and import all pages in App

--- 09-mixmaster/mixmaster/src/App.jsx
+++ 09-mixmaster/mixmaster/src/App.jsx
@@ -1,17 +1,21 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Landing,
+  About,
+  Cocktail,
+  Newsletter,
+  HomeLayout,
+  Error,
+} from "./pages";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <h2>Home Page</h2>,
+    element: <HomeLayout />,
   },
   {
     path: "/about",
-    element: (
-      <div>
-        <h2>About Page</h2>
-      </div>
-    ),
+    element: <About />,
   },
 ]);
 
