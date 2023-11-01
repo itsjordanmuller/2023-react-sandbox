33fc2f367c6cbeee55ea3ff38118889f496be17d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-10-31 22:29:00 -0700

    Create HTML form to handle newsletter sign-up submissions, swap to Form component and set action at route in App file

--- 09-mixmaster/mixmaster/src/pages/Newsletter.jsx
+++ 09-mixmaster/mixmaster/src/pages/Newsletter.jsx
@@ -1,4 +1,69 @@
+import { Form, redirect } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+const newsletterUrl = "https://www.course-api.com/cocktails-newsletter";
+
+export const action = async ({ request }) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  const response = await axios.post(newsletterUrl, data);
+
+  toast.success(response.data.msg);
+  return redirect("/");
+};
+
 const Newsletter = () => {
-  return <h1>Newsletter</h1>;
+  return (
+    <Form className="form" method="POST">
+      <h4 style={{ textAlign: "center", marginBottom: "2rem" }}>Newsletter</h4>
+      {/* first name */}
+      <div className="form-row">
+        <label htmlFor="name" className="form-label">
+          name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="name"
+          id="name"
+          defaultValue="John"
+        />
+      </div>
+      {/* last name */}
+      <div className="form-row">
+        <label htmlFor="lastName" className="form-label">
+          last name
+        </label>
+        <input
+          type="text"
+          className="form-input"
+          name="lastName"
+          id="lastName"
+          defaultValue="Smith"
+        />
+      </div>
+      {/* email */}
+      <div className="form-row">
+        <label htmlFor="email" className="form-label">
+          email
+        </label>
+        <input
+          type="email"
+          className="form-input"
+          name="email"
+          id="email"
+          defaultValue="test@test.com"
+        />
+      </div>
+      <button
+        type="submit"
+        className="btn btn-block"
+        style={{ marginTop: "0.5rem" }}
+      >
+        Submit
+      </button>
+    </Form>
+  );
 };
 export default Newsletter;
