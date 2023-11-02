700d652c9f41d6f6345b00a727660f09ac725756
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-01 20:18:54 -0700

    Calculate the totals for the items currently in the cart using Effect Hook in App file

--- 11-redux-toolkit-tutorial/redux-toolkit-tutorial/src/App.js
+++ 11-redux-toolkit-tutorial/redux-toolkit-tutorial/src/App.js
@@ -1,7 +1,16 @@
 import CartContainer from "./components/CartContainer";
 import Navbar from "./components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
+import { useEffect } from "react";
 
 function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems, dispatch]);
   return (
     <main>
       <Navbar />
