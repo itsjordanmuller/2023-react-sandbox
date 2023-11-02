1afc76bd58c57fb46a8979f57e954bbd17587896
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-01 19:58:39 -0700

    Setup a reducer to clear the cart using the clear cart button

--- 11-redux-toolkit-tutorial/redux-toolkit-tutorial/src/components/CartContainer.js
+++ 11-redux-toolkit-tutorial/redux-toolkit-tutorial/src/components/CartContainer.js
@@ -1,7 +1,9 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { cartItems, total, amount } = useSelector((store) => store.cart);
 
   if (amount < 1) {
@@ -32,7 +34,9 @@ const CartContainer = () => {
             total <span>${total}</span>
           </h4>
         </div>
-        <button className="btn clear-btn">Clear Cart</button>
+        <button className="btn clear-btn" onClick={() => dispatch(clearCart())}>
+          Clear Cart
+        </button>
       </footer>
     </section>
   );
