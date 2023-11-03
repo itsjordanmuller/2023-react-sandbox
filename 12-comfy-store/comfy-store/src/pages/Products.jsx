6322b1d63d38978e97425e5a9cc0d43c0fd58705
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 12:51:11 -0700

    Setup the process of getting and setting parameters from the products form, set the defaultValues of those inputs to the params in the URL

--- 12-comfy-store/comfy-store/src/pages/Products.jsx
+++ 12-comfy-store/comfy-store/src/pages/Products.jsx
@@ -4,10 +4,16 @@ import { customFetch } from "../utils";
 const url = "/products";
 
 export const loader = async ({ request }) => {
-  const response = await customFetch(url);
+  const params = Object.fromEntries([
+    ...new URL(request.url).searchParams.entries(),
+  ]);
+  console.log(params);
+  const response = await customFetch(url, {
+    params,
+  });
   const products = response.data.data;
   const meta = response.data.meta;
-  return { products, meta };
+  return { products, meta, params };
 };
 
 const Products = () => {
