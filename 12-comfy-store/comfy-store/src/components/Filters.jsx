e14124ea820eef8576761723802de5c4921379ae
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 12:16:02 -0700

    Create and use FormSelect input components in the Filters component to add selections for category, company, and sort

--- 12-comfy-store/comfy-store/src/components/Filters.jsx
+++ 12-comfy-store/comfy-store/src/components/Filters.jsx
@@ -1,7 +1,10 @@
 import { Form, useLoaderData, Link } from "react-router-dom";
 import FormInput from "./FormInput";
+import FormSelect from "./FormSelect";
 
 const Filters = () => {
+  const { meta } = useLoaderData();
+  console.log(meta);
   return (
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4 gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
@@ -11,6 +14,27 @@ const Filters = () => {
         name="search"
         size="input-sm"
       ></FormInput>
+      {/* CATEGORIES */}
+      <FormSelect
+        label="select category"
+        name="category"
+        list={meta.categories}
+        size="select-sm"
+      />
+      {/* COMPANIES */}
+      <FormSelect
+        label="select company"
+        name="company"
+        list={meta.companies}
+        size="select-sm"
+      />
+      {/* ORDER */}
+      <FormSelect
+        label="sort by"
+        name="order"
+        list={["A-Z", "Z-A", "Highest $", "Lowest $"]}
+        size="select-sm"
+      />
       {/* BUTTONS */}
       <button type="submit" className="btn btn-primary btn-sm">
         search
