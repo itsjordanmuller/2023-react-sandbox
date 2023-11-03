64bde0e42d3a1993e79c5ec9732f854e5f83dca9
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 13:34:12 -0700

    Create a cartSlice and store file to setup Redux Toolkit for tracking cartState

---
+++ 12-comfy-store/comfy-store/src/features/cart/cartSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
+
+const defaultState = {
+  cartItems: [],
+  numItemsInCart: 0,
+  cartTotal: 0,
+  shipping: 500,
+  tax: 0,
+  orderTotal: 0,
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: defaultState,
+  reducers: {
+    addItem: (state, action) => {
+      console.log(action.payload);
+    },
+    removeItem: (state) => {},
+    editItem: (state) => {},
+    clearCart: (state) => {},
+  },
+});
+
+export const { addItem, removeItem, editItem, clearCart } = cartSlice.actions;
+
+export default cartSlice.reducer;
