fb0ecd9b5ac8e29133f990c4111cbf63c3d88c0a
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-02 19:02:28 -0700

    Continue work on the SingleProduct page/component including colors, amount, and add to cart button

--- 12-comfy-store/comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/comfy-store/src/pages/SingleProduct.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import { formatPrice, customFetch } from "../utils";
+import { formatPrice, customFetch, generateAmountOptions } from "../utils";
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
@@ -15,6 +15,11 @@ const SingleProduct = () => {
     product.attributes;
   const dollarsAmount = formatPrice(price);
   const [productColor, setProductColor] = useState(colors[0]);
+  const [amount, setAmount] = useState(1);
+
+  const handleAmount = () => {
+    setAmount(parseInt(e.target.value));
+  };
   return (
     <section>
       <div className="text-md breadcrumbs">
@@ -63,6 +68,30 @@ const SingleProduct = () => {
                 );
               })}
             </div>
+          </div>
+          {/* AMOUNT */}
+          <div className="form-control w-full max-w-xs">
+            <label className="label" htmlFor="amount">
+              <h4 className="text-md font-medium tracking-wider capitalize">
+                amount
+              </h4>
+            </label>
+            <select
+              className="select select-secondary select-bordered select-md"
+              value={amount}
+              onChange={handleAmount}
+            >
+              {generateAmountOptions(5)}
+            </select>
+          </div>
+          {/* CART BUTTON */}
+          <div className="mt-10 ">
+            <button
+              className="btn btn-secondary btn-md"
+              onClick={() => console.log("add to bag")}
+            >
+              Add to bag
+            </button>
           </div>
         </div>
       </div>
