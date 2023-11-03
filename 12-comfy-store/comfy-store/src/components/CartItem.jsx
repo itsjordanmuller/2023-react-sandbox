5644a3f7117a557e050da0f24e4541147776bf7d
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 14:17:41 -0700

    Create a grid layout for the Cart page, set default user to null for now

---
+++ 12-comfy-store/comfy-store/src/components/CartItem.jsx
@@ -0,0 +1,4 @@
+const CartItem = () => {
+  return <h1 className="text-3xl">CartItem</h1>;
+};
+export default CartItem;
---
+++ 12-comfy-store/comfy-store/src/components/CartItemsList.jsx
@@ -0,0 +1,4 @@
+const CartItemsList = () => {
+  return <h1 className="text-3xl">CartItemsList</h1>;
+};
+export default CartItemsList;
