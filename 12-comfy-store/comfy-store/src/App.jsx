4380aeba3fed54bfc661ae5b21555b9e5534fd83
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 20:18:38 -0700

    Begin to layout the React Router From component with custom FormInput and SubmitBtn components

--- 12-comfy-store/comfy-store/src/App.jsx
+++ 12-comfy-store/comfy-store/src/App.jsx
@@ -25,7 +25,7 @@ import { loader as checkoutLoader } from "./pages/Checkout";
 // actions
 import { action as registerAction } from "./pages/Register";
 import { action as loginAction } from "./pages/Login";
-
+import { action as checkoutAction } from "./components/CheckoutForm";
 import { store } from "./store";
 
 const router = createBrowserRouter([
@@ -64,6 +64,7 @@ const router = createBrowserRouter([
         path: "/checkout",
         element: <Checkout />,
         loader: checkoutLoader(store),
+        action: checkoutAction(store),
       },
       {
         path: "/orders",
