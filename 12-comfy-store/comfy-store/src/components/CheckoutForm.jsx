b17ad140fee69ef233e5c505c61d680c64b0d6dd
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 20:29:37 -0700

    Implement ability to send a POST request with the cart information to place an order with those items, handle basic errors with catch

--- 12-comfy-store/comfy-store/src/components/CheckoutForm.jsx
+++ 12-comfy-store/comfy-store/src/components/CheckoutForm.jsx
@@ -5,10 +5,46 @@ import { customFetch, formatPrice } from "../utils";
 import { toast } from "react-toastify";
 import { clearCart } from "../features/cart/cartSlice";
 
-export const action = (store) => async () => {
-  console.log(store);
-  return null;
-};
+export const action =
+  (store) =>
+  async ({ request }) => {
+    const formData = await request.formData();
+    const { name, address } = Object.fromEntries(formData);
+
+    const user = store.getState().userState.user;
+    const { cartItems, orderTotal, numItemsInCart } =
+      store.getState().cartState;
+
+    const info = {
+      name,
+      address,
+      chargeTotal: orderTotal,
+      orderTotal: formatPrice(orderTotal),
+      cartItems,
+      numItemsInCart,
+    };
+
+    try {
+      const response = await customFetch.post(
+        "/orders",
+        { data: info },
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      store.dispatch(clearCart());
+      toast.success("Order placed successfully");
+      return redirect("/orders");
+    } catch (error) {
+      const errorMessage =
+        error?.response?.data?.error?.message ||
+        "There was an error placing your order";
+      toast.error(errorMessage);
+      return null;
+    }
+  };
 
 const CheckoutForm = () => {
   return (
