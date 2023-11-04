79aef9176b702f8fa077ca036bd193adaed964cb
itsjordanmuller <itsjordanmuller@gmail.com>
2023-11-03 21:15:00 -0700

    Add React Query & ReactQueryDevTools to App file, create new QueryClient instance, and pass it to QueryClientProvider wrapped around RouterProvider
    Modify Landing, Products and SingleProducts, as well as checkout and orderloaders and actions

--- 12-comfy-store/comfy-store/src/pages/SingleProduct.jsx
+++ 12-comfy-store/comfy-store/src/pages/SingleProduct.jsx
@@ -5,11 +5,13 @@ import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addItem } from "../features/cart/cartSlice";
 
-export const loader = async ({ params }) => {
-  const response = await customFetch(`/products/${params.id}`);
+export const loader =
+  (queryClient) =>
+  async ({ params }) => {
+    const response = await customFetch(`/products/${params.id}`);
 
-  return { product: response.data.data };
-};
+    return { product: response.data.data };
+  };
 
 const SingleProduct = () => {
   const { product } = useLoaderData();
